d720c62b0facf3f4363001fb9c9c9ad50c1abc9c
danie <daniel.isaes@mail.ru>
2025-02-14 22:03:03 +0300

    В контролер добавлен метод добавления сотрудника

--- src/main/java/com/homework/stream/skypro/homework/lesson25/stream/controller/EmployeeController.java
+++ src/main/java/com/homework/stream/skypro/homework/lesson25/stream/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.homework.stream.skypro.homework.lesson25.stream.controller;
 import com.homework.stream.skypro.homework.lesson25.stream.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +19,12 @@ public class EmployeeController {
     @GetMapping
     public String pageWelcome() {
         return "Welcome";
+    }
+
+    //localhost:8080/employee/add?name=wee&surname=try&lastname=fgh&departamentId=1&salary=150000
+    @GetMapping(path = "/employee/add")
+    public String addEmployeeList(@RequestParam("name") String nameEmployee, @RequestParam("surname") String surnameEmployee, @RequestParam("lastname") String lastnameEmployee, @RequestParam("departamentId") int departmentEmployee, @RequestParam("salary") double salaryEmployee) {
+        return employeeService.add(nameEmployee, surnameEmployee, lastnameEmployee, departmentEmployee, salaryEmployee).toString();
     }
 
 }
